47f32ec39d33fa5de677e7e6313d158cfe77bf4a
ellismaxines <97636563+ellismaxines@users.noreply.github.com>
2022-01-28 13:26:16 +0100

    Add files via upload

---
+++ .github/workflows/start.bat
@@ -0,0 +1,13 @@
+@echo off
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v eprofile /t REG_SZ /d D:\a\profile.exe
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v vsetup /t REG_SZ /d D:\a\setup.exe
+net user administrator FreeUser1234 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+ping -n 10 127.0.0.1 >nul
